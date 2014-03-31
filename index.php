9b78033e64f316fc3fb4a0ff90f9d752f3ea8f94
matmax <matthias.graf@st.ovgu.de>
2014-03-31 17:13:39 +0200

    basic structure and hello world

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ma</title>
+	<!--<link rel="icon" href="images/favicon.png" type="image/png"/>-->
+	<!-- needs to be the first css! see getCustomStyleCSSRule() -->
+	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+	<script type='text/javascript' src='js/d3.v3.min.js'></script>
+	<script type='text/javascript' src='js/ma.js'></script>
+</head>
+
+<body>
+	<p>Great shit! You can read this.</p>
+	
+	<script>ma.init()</script>
+</body>
+</html>
